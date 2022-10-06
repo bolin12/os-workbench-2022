499673a8ce8de7a2876ba6bd7adf7e2ae1f40e68
tracer-nju <tracer@nju.edu.cn>
2022-10-06 16:13:46 +0800

    Linux lbl-GE63-Raider-RGB-8SG 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:13:46 up  1:14,  1 user,  load average: 0.37, 0.53, 0.64

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -143,10 +143,9 @@ void print_pstree() {
 void proctree_print(int pid, int step){
     
     for(int i=0;i<proc_count;i++){
-        // printf("ppid==:%d\n",proc[i].ppid);
         if(proc[i].ppid == pid){
             for(int j=0;j<step;j++){
-                printf("      ");
+                printf("            ");
             }
             printf("%s%d\n",  proc[i].proc_name,proc[i].pid);
             proctree_print(proc[i].pid, step+1);
